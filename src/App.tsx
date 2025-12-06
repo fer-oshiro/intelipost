613070f072cb4fba43cbc48270aeb7af9ca18462
Fernanda Oshiro <fernanda.sayuri@outlook.com>
2025-12-06 14:32:27 -0300

    chore: add react template

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <main>Intelipost</main>
+      <footer>
+        <p>Teste de Frontend Intelipost • Desenvolvido por Fernanda Oshiro</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
