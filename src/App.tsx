968f255cf981f016bf84097d8dd2feac6842431d
Fernanda Oshiro <fernanda.sayuri@outlook.com>
2025-12-06 17:24:31 -0300

    chore: set up initial project structure with styles and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 function App() {
   return (
-    <>
-      <main>Intelipost</main>
-      <footer>
+    <div className="layout">
+      <main className="layout__main">
+        <h1>Intelipost</h1>
+      </main>
+      <footer className="layout__footer">
         <p>Teste de Frontend Intelipost • Desenvolvido por Fernanda Oshiro</p>
       </footer>
-    </>
+    </div>
   );
 }
 
