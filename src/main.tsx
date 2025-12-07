7a55a5c0783c4164ade10dde26a3db8b7be5af2e
Fernanda Oshiro <fernanda.sayuri@outlook.com>
2025-12-07 20:40:17 -0300

    feat: add ArtistSearchCombobox component with styles and integrate TicketMaster suggestions

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
 import './styles/main.scss';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SearchProvider } from './context/search.tsx';
 
-const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <SearchProvider>
+        <App />
+      </SearchProvider>
     </QueryClientProvider>
   </StrictMode>
 );
